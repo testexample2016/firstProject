ad90cf126eee534795e3504876dba95a7956f175
testexample2016 <testexample2016@gmail.com>
2018-10-10 17:40:24 +0530

    update on 10 oct 2018
    dashboard css updated

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,11 +9,13 @@
 
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
+     <link rel="stylesheet" href="{{URL::to('src/css/main.css') }}">
+
      <style>
      
-input:invalid {
+/*input:invalid {
   background-color: #ffdddd;
-}
+}*/
     </style>
 
        
